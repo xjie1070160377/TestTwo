e368c1befaa288051db82ddf130bdad7501e33f4
jjboy <1070160377@qq.com>
2016-08-10 23:24:32 +0800

    第一次提交

--- app/src/main/java/com/example/xj/testtwo/MainActivity.java
+++ app/src/main/java/com/example/xj/testtwo/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.xj.testtwo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.xj.main.FirstActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void myOnclick(View view) {
+        Intent intent = new Intent(this, FirstActivity.class);
+        startActivity(intent);
     }
 }
